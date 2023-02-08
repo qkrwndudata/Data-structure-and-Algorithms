1369e703a3e79df7625934982a13dcefe979979f
dudwnqkr <79184083+qkrwndudata@users.noreply.github.com>
2023-02-08 22:25:30 +0900

    Update binary search1.py

--- Search/binary search1.py
+++ Search/binary search1.py
@@ -8,12 +8,12 @@ Original file is located at
 """
 
 def binary_search(array, target, start, end):
-		while start <= end:
-				mid = (start + end) // 2
-				if array[mid] == target:
-						return mid
-				elif array[mid] > target:
-						end = mid - 1
-				else:
-						start = mid + 1
-				return None
+	while start <= end:
+		mid = (start + end) // 2
+		if array[mid] == target:
+			return mid
+		elif array[mid] > target:
+			end = mid - 1
+		else:
+			start = mid + 1
+		return None
